bebfb7d6939fd76d584c59f8b2f5def89b911ae0
Muruga <muruk@Murugas-MacBook-Pro.local>
2025-12-03 10:59:29 +0530

    memo list

--- src/lib/graphql/query/sales.ts
+++ src/lib/graphql/query/sales.ts
@@ -51,6 +51,68 @@ export const GET_SALES_INVOICE_LIST_QUERY = gql`
   }
 `;
 
+export const GET_MEMO_LIST_QUERY = gql`
+  query GetMemoList(
+    $storeid: Int!
+    $outletid: Int!
+    $warehouseid: Int
+    $page: Int!
+    $perpage: Int!
+    $filters: [FilterKeyValuePair]
+    $sortModel: [SortModelInput]
+    $rowGroupCols: [RowGroupColInput]
+    $groupKeys: [String]
+  ) {
+    getMemoList(
+      storeid: $storeid
+      outletid: $outletid
+      warehouseid: $warehouseid
+      page: $page
+      perpage: $perpage
+      filters: $filters
+      sortModel: $sortModel
+      rowGroupCols: $rowGroupCols
+      groupKeys: $groupKeys
+    ) {
+      total
+      data {
+        memonumber
+        customerid
+        companyname
+        saledate
+        salemodename
+        numberofitems
+        totalamount
+        discountamount
+        subtotal
+        salestax
+        shipping
+        netamount
+        amountreceived
+        balancedue
+        termsname
+        warehousename
+        warehouseid
+        outletid
+        createby
+        registerno
+        isweborder
+        invsalesorder
+        voiddate
+        lastmodifiedby
+        lastmodifieddate
+      }
+      totalsRow {
+        totalamount
+        subtotal
+        netamount
+        amountreceived
+        balancedue
+      }
+    }
+  }
+`;
+
 export const GET_SALES_ORDER_LIST_QUERY = gql`
   query GetSalesOrderList(
     $outletid: Int!
